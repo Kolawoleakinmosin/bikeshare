ba6a40101489f5ab19a9ade0bb87bfa80735d71e
Karmen de Reuck <dereuckkarmen@gmail.com>
2023-03-13 16:47:09 +0200

    bike model

---
+++ db/migrate/20230313144059_create_bikes.rb
@@ -0,0 +1,11 @@
+class CreateBikes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bikes do |t|
+      t.string :location
+      t.integer :price
+      t.boolean :availability || false
+      t.references :user
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_13_144059) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bikes", force: :cascade do |t|
+    t.string "location"
+    t.integer "price"
+    t.boolean "availability"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_bikes_on_user_id"
+  end
 
 end
