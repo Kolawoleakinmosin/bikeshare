dda030c769466cc9e35ee3e8063381ee1f93008e
Karmen de Reuck <dereuckkarmen@gmail.com>
2023-03-14 17:23:18 +0200

    navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "bikes#index"
+  get "/my-bikes", to: "bikes#mybikes"
+
   resources :bikes, except: [:index] do
     resources :bookings, only: %i[create new]
   end
