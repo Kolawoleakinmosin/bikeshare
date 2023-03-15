e42b1651a437f5686755091a346eccad4d8cc017
kolawole akinmosin <kolawoleakinmosin01@gmail.com>
2023-03-15 14:58:50 +0000

    adding availability

--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_14_155324) do
   create_table "bikes", force: :cascade do |t|
     t.string "location"
     t.integer "price"
-    t.boolean "availability", default: false
+    t.boolean "availability", default: true
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
